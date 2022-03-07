1345d0186db609b7f51bcb399c0f9dffc8764f28
angelalk <angelalk@ciencias.unam.mx>
2022-03-07 10:48:33 -0600

    second part from third lab

---
+++ practica03/src/Parte2.exs
@@ -0,0 +1,58 @@
+# Participantes:
+# Angel Alcántara Valdés
+# Mauricio Ayala Morales
+# Hernández Sanchez Oscar Jose
+# Madera Baldovinos Erika Yusset
+
+defmodule Parte2 do
+  defp reverse([],rev) do
+	rev
+  end
+  defp reverse([h|t], rev) do
+	reverse(t, [h|rev])
+  end
+  defp reverse(l) when is_list(l) do
+	reverse(l,[])
+  end
+
+  #1
+  defp split_rec(left, [], _count) do
+	{reverse(left),[]}
+  end
+  defp split_rec(left, right, 0) do
+	{reverse(left), right}
+  end
+  defp split_rec(left, [r|right], count) do
+	split_rec([r|left], right, count-1)
+  end
+  def split(l, split_ind) when is_list(l) and is_number(split_ind) do
+	split_rec([], l, split_ind)
+  end
+
+  #2
+  def all?([], _fun) do
+	true
+  end
+  def all?([h|t], fun) when is_function(fun) do
+	if fun.(h) do
+	  all?(t,fun)
+	else
+	  false
+	end
+  end
+
+  #3
+  defp filter_rec([], res, _test) do
+	reverse(res)
+  end
+  defp filter_rec([h|t], res, test) do
+	if test.(h) do
+	  filter_rec(t, [h|res], test)
+	else
+	  filter_rec(t, res, test)
+	end
+  end
+  def filter(l, test) when is_list(l) and is_function(test) do
+	filter_rec(l,[],test)
+  end
+end
