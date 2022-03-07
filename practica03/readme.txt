3466fb9218e885f4dda3c7362c131df614d83e05
ContrarioMotu <contrariomotu@ciencias.unam.mx>
2022-03-06 19:40:29 -0600

    Subiendo Parte 3 y readme.

---
+++ practica03/readme.txt
@@ -0,0 +1,7 @@
+Participantes:
+- Alcántara Valdés Angel
+- Ayala Morales Mauricio
+- Hernandez Sanchez Oscar José
+- Madera Baldovinos Erika Yusset
+
+No tuvimos ningún problema con la práctica.
---
+++ practica03/src/Parte3.exs
@@ -0,0 +1,29 @@
+# Participantes:
+# Angel Alcántara Valdés
+# Mauricio Ayala Morales
+# Hernández Sanchez Oscar Jose
+# Madera Baldovinos Erika Yusset
+
+defmodule Parte3 do
+
+  # Función para leer los mensajes recibidos:
+  #     (a) Si recibe una tupla con el átomo :pid deberá imprimir lo siguiente:
+  #         "Hola PIDA, soy PIDB." donde PIDA es el pid que te manden, y
+  #         PIDB es el PIDB del proceso. Solo recibes una tupla de la forma:
+  #         {:pid, self()}.
+  #     (b) Si recibe una tupla con el átomo :ok deberá imprimir el mensaje
+  #         que traiga la segunda entrada de la tupla.
+  #     (c) Si recibe una tupla con el átomo :nok deberá imprimir el mensaje
+  #         concatenado a si mismo.
+  def recibe() do
+    receive do
+      {:pid, str} -> if is_pid(str) do IO.puts("Hola #{inspect(str)}, soy #{inspect(self())}") end
+                     recibe()
+      {:ok, str} -> if is_binary(str) do IO.puts(str) end
+                    recibe()
+      {:nok, str} -> if is_binary(str) do IO.puts(str <> str) end
+                     recibe()
+    end
+  end
+
+end
