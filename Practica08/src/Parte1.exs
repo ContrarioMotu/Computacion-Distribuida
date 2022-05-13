93f4e37bb06be04e43da03ebf1c284a5c225ed56
ContrarioMotu <contrariomotu@ciencias.unam.mx>
2022-05-13 00:48:49 -0500

    Subiendo readme.txt, imagen de la gráfica y añadiendo comentarios a la práctica.

--- Practica08/src/Parte1.exs
+++ Practica08/src/Parte1.exs
@@ -1,9 +1,18 @@
+# Participantes:
+# Angel Alcántara Valdés
+# Mauricio Ayala Morales
+# Hernández Sanchez Oscar Jose
+# Madera Baldovinos Erika Yusset
 defmodule LeaderElection do
+
+  # Función para "inicializar" un proceso con un estado por default.
   def inicia do
     estado = %{termino?: false}
     recibe_mensaje(estado)
   end
 
+  # Función que permite a un proceso recibir mensajes, tanto para cambiar
+  # como para imprimir su estado.
   def recibe_mensaje(estado) do
     if not estado[:termino?] do
       receive do
@@ -13,22 +22,31 @@ defmodule LeaderElection do
     end
   end
 
-  ##Setup
+  # Función para asignarle un id a un proceso.
   def procesa_mensaje({:id, id}, estado) do
     Map.put(estado, :id, id)
   end
+
+  # Función para asignarle vecinos a un proceso.
   def procesa_mensaje({:vecinos, izquierdo, derecho}, estado) do
     estado = Map.put(estado, :derecho, derecho)
     Map.put(estado, :izquierdo, izquierdo)
   end
 
-  ##Run
+  # Función para inicializar el algoritmo de elección
+  # del lídel desde cualquier proceso.
   def procesa_mensaje({:inicia}, estado) do
     self_id = estado[:id]
     send(estado[:izquierdo], {:compare, self_id})
     Map.put(estado, :lider, self_id)
   end
 
+  # Función para que un proceso compare su id con un id
+  # recibido.
+  #   Si el id recibido es mayor al suyo, reenvía
+  #   el id recibido a su vecino izquierdo, si el recibido
+  #   es igual al suyo el proceso se proclama líder, en
+  #   cualquier otro caso no hace nada.
   def procesa_mensaje({:compare, other_id}, estado) do
     self_id = estado[:id]
     cond do
@@ -43,6 +61,8 @@ defmodule LeaderElection do
     end
   end
 
+  # Función para que un proceso se proclame líder y le avise
+  # a los demás procesos.
   def procesa_mensaje({:lider}, estado) do
     IO.puts "Soy #{estado[:id]} y el lider es #{estado[:lider]}"
     send(estado[:izquierdo], {:lider})
